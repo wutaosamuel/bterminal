2cfce20d2ae6a4d7be050df727d007e79947d3d5
wutao <497863986@qq.com>
2020-02-21 07:09:22 +0000

    add function now

--- html/index.go
+++ html/index.go
@@ -6,7 +6,10 @@ package html
 
 import (
 	"fmt"
+	//"io"
+	//"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 // HandleIndex is process functions of index.html
@@ -14,17 +17,37 @@ import (
 // Cookie
 // Config
 func HandleIndex(w http.ResponseWriter, req *http.Request) {
+	//http.ServeFile(w, req, "html/")
 	req.ParseForm()
-	fmt.Println(req.Form)
-	fmt.Println("path ", req.URL.Path)
-	fmt.Println("scheme ", req.URL.Scheme)
+	PrintHTMLInfo(req)
 
-	// not all value pass by GET URL
+	// First time access server
+	// GET / URL == '/' for index page
 	if req.Method == "GET" {
-		// FIXME: potential fail
-		http.ServeFile(w, req, "./index.html")
+		fmt.Println("handle index method get")
+		//indexHTML, _ := ioutil.ReadFile("html/index.html")
+		//fmt.Fprintf(w, string(indexHTML))
+		http.ServeFile(w, req, "html/index.html")
 	}
 
-	fmt.Println(req.Form["password"])
-	http.ServeFile(w, req, "./jobs.html")
+	// Form will require by POST
+	for i, value := range req.Form {
+		fmt.Println("key: ", i)
+		fmt.Println("value: ", strings.Join(value, ""))
+	}
+	if req.Method == "POST" {
+		// FIXME: potential fail
+		fmt.Println(req.Form["password"])
+		http.ServeFile(w, req, "html/shell.html")
+	}
+}
+
+// TODO: base64 decode
+
+// PrintHTMLInfo infomation
+func PrintHTMLInfo(req *http.Request){
+	fmt.Println(req.Form)
+	fmt.Println("path: ", req.URL.Path)
+	fmt.Println("scheme: ", req.URL.Scheme)
+	fmt.Println("method: ", req.Method)
 }
