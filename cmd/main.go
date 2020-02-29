72c6f907aa80bf61e8cea34557322b01842ff99c
wutao <497863986@qq.com>
2020-02-29 20:33:24 +0800

    done execaction test && need to test cron action

--- cmd/main.go
+++ cmd/main.go
@@ -13,13 +13,13 @@ import (
 )
 
 func main() {
-
+	// read from cli
 	var (
 		helpFlag       = pflag.BoolP("help", "h", false, "display usage")
 		configPathFlag = pflag.StringP("config", "c", "", "Json config file")
 		portFlag       = pflag.IntP("port", "p", 5122, "TCP port for web service")
 		passwordFlag   = pflag.StringP("password", "P", "", "password for protecting web service")
-		logDirFlag     = pflag.StringP("log", "l", "./", "log directory for keeping job logs")
+		logDirFlag     = pflag.StringP("log", "l", "", "log directory for keeping job logs")
 	)
 
 	pflag.Parse()
@@ -33,6 +33,7 @@ func main() {
 	fmt.Println(*passwordFlag)
 	fmt.Println(*logDirFlag)
 
+	// set config 
 	config := conf.NewConfig()
 	config.Path = *configPathFlag
 	config.Port = strconv.Itoa(*portFlag)
@@ -40,8 +41,10 @@ func main() {
 	config.LogDir = *logDirFlag
 	config.Init()
 
+	// set App path
 	thisPath,_ := os.Getwd()
 	appPath,_ := filepath.Abs(filepath.Dir(thisPath))
-	fmt.Println(appPath)
+
+	// do main func
 	bt.Main(config, appPath)
 }
